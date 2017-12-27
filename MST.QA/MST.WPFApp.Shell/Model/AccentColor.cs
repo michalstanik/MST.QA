4bac8f56e59cc15b6fc5e88f965dae52cd97e0f3
Michał Stanik <micha.stanik@gmail.com>
2017-12-28 00:53:23 +0100

    Color Change

---
+++ MST.QA/MST.WPFApp.Shell/Model/AccentColor.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using System.Windows.Media;
+
+namespace MST.WPFApp.Shell.Model
+{
+    public class AccentColor : BindableBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.SetProperty<string>(ref this.name, value); }
+        }
+
+        private Brush colorBrush;
+
+        public Brush ColorBrush
+        {
+            get { return colorBrush; }
+            set { this.SetProperty<Brush>(ref this.colorBrush, value); }
+        }
+    }
+}
--- MST.QA/MST.WPFApp.Shell/ViewModels/ShellSettingsFlyoutViewModel.cs
+++ MST.QA/MST.WPFApp.Shell/ViewModels/ShellSettingsFlyoutViewModel.cs
@@ -19,9 +19,14 @@ namespace MST.WPFApp.Shell.ViewModels
         public ShellSettingsFlyoutViewModel(IGlobalConfigService globalConfigService)
         {
             _globalConfigService = globalConfigService;
+
             this.ApplicationThemes = ThemeManager.AppThemes
                 .Select(a => new ApplicationTheme() { Name = a.Name, BorderColorBrush = a.Resources["BlackColorBrush"] as Brush, ColorBrush = a.Resources["WhiteColorBrush"] as Brush })
                 .ToList();
+
+            this.AccentColors = ThemeManager.Accents
+                                .Select(a => new AccentColor() { Name = a.Name, ColorBrush = a.Resources["AccentColorBrush"] as Brush })
+                                .ToList();
         }
 
         private IList<ApplicationTheme> applicationsThemes;
@@ -30,6 +35,14 @@ namespace MST.WPFApp.Shell.ViewModels
         {
             get { return applicationsThemes; }
             set { this.SetProperty<IList<ApplicationTheme>>(ref this.applicationsThemes, value); }
+        }
+
+        private IList<AccentColor> accentColors;
+
+        public IList<AccentColor> AccentColors
+        {
+            get { return accentColors; }
+            set { this.SetProperty<IList<AccentColor>>(ref this.accentColors, value); }
         }
 
         private ApplicationTheme selectedTheme;
@@ -48,6 +61,24 @@ namespace MST.WPFApp.Shell.ViewModels
                     _globalConfigService.Update(AppSettingsParam.AppTheme, appTheme.Name);
 
                     EventAggregator.GetEvent<StatusBarMessageUpdateEvent>().Publish(String.Format("Theme changed to {0}", value.Name));
+                }
+            }
+        }
+
+        private AccentColor selectedAccentColor;
+
+        public AccentColor SelectedAccentColor
+        {
+            get { return selectedAccentColor; }
+            set
+            {
+                if (this.SetProperty<AccentColor>(ref this.selectedAccentColor, value))
+                {
+                    var theme = ThemeManager.DetectAppStyle(Application.Current);
+                    var accent = ThemeManager.GetAccent(value.Name);
+                    ThemeManager.ChangeAppStyle(Application.Current, accent, theme.Item1);
+
+                    EventAggregator.GetEvent<StatusBarMessageUpdateEvent>().Publish(String.Format("Accent color changed to {0}", value.Name));
                 }
             }
         }
